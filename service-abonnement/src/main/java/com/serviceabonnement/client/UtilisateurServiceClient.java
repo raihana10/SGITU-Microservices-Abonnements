a56648ada72f2d8e459daf5f470fe9cbd6d330ef
AmineElBiyadi <amine.elbiyadi.8@gmail.com>
2026-06-03 10:52:25 +0100

    feat: configure Feign to forward auth headers
    add a Feign RequestInterceptor that copies the Authorization header from the original incoming request to downstream service calls, then update the UtilisateurServiceClient to use this configuration

--- service-abonnement/src/main/java/com/serviceabonnement/client/UtilisateurServiceClient.java
+++ service-abonnement/src/main/java/com/serviceabonnement/client/UtilisateurServiceClient.java
@@ -1,5 +1,6 @@
 package com.serviceabonnement.client;
 
+import com.serviceabonnement.config.FeignConfig;
 import com.serviceabonnement.dto.external.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
-@FeignClient(name = "service-utilisateur", url = "${application.config.utilisateur-service-url}")
+@FeignClient(name = "service-utilisateur", url = "${application.config.utilisateur-service-url}", configuration = FeignConfig.class)
 public interface UtilisateurServiceClient {
 
     @GetMapping("/api/users/{id}")
--- service-abonnement/src/main/java/com/serviceabonnement/config/FeignConfig.java
+++ service-abonnement/src/main/java/com/serviceabonnement/config/FeignConfig.java
@@ -1,8 +1,12 @@
 package com.serviceabonnement.config;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 public class FeignConfig {
@@ -10,5 +14,19 @@ public class FeignConfig {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                String authorizationHeader = request.getHeader("Authorization");
+                if (authorizationHeader != null) {
+                    requestTemplate.header("Authorization", authorizationHeader);
+                }
+            }
+        };
     }
 }
